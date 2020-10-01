2d96440162ab8cd23aab89a619d9f9562a1c4818
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2020-10-01 12:44:33 -0400

    Title change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		title: `OnTouch`,
+		description: `Test site`,
 		author: `Yoelvis Oliveros`,
 	},
 	plugins: [
